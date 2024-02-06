8bd9a405cca48bb030da4739e232b63093766233
Jared Cazel <jjcazel@gmail.com>
2024-02-06 11:37:58 -0500

    Add API details and types. Imported React Query to wrap the body of the app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,68 @@
 import Navbar from "@/components/Navbar";
-import Image from "next/image";
+
+// https://api.openweathermap.org/data/2.5/forecast?q=keyport&appid=0bf1bddb1098291128c414cdb6eec85c
+interface Main {
+  temp: number;
+  feels_like: number;
+  temp_min: number;
+  temp_max: number;
+  pressure: number;
+  sea_level: number;
+  grnd_level: number;
+  humidity: number;
+  temp_kf: number;
+}
+interface Weather {
+  id: number;
+  main: string;
+  description: string;
+  icon: string;
+}
+
+interface Clouds {
+  all: number;
+}
+interface Wind {
+  speed: number;
+  deg: number;
+  gust: number;
+}
+
+interface Sys {
+  pod: string;
+}
+interface ListItem {
+  dt: number;
+  main: Main;
+  weather: Weather[];
+  clouds: Clouds;
+  wind: Wind;
+  visibility: number;
+  pop: number;
+  sys: Sys;
+  dt_txt: string;
+}
+interface Coord {
+  lat: number;
+  lon: number;
+}
+interface City {
+  id: number;
+  name: string;
+  coord: Coord;
+  country: string;
+  population: number;
+  timezone: number;
+  sunrise: number;
+  sunset: number;
+}
+interface WeatherData {
+  cod: string;
+  message: number;
+  cnt: number;
+  list: ListItem[];
+  city: City;
+}
 
 export default function Home() {
   return (
@@ -8,3 +71,4 @@ export default function Home() {
     </div>
   );
 }
+
