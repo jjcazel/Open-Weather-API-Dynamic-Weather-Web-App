a6d5e7753ce1414e3bf984ef7edfaf85825fd5dd
Jared Cazel <jjcazel@gmail.com>
2024-02-08 11:21:26 -0500

    Import Axios and use env variable to make open weather api call

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,74 +1,83 @@
+"use client";
+
 import Navbar from "@/components/Navbar";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
 // https://api.openweathermap.org/data/2.5/forecast?q=keyport&appid=0bf1bddb1098291128c414cdb6eec85c
-interface Main {
-  temp: number;
-  feels_like: number;
-  temp_min: number;
-  temp_max: number;
-  pressure: number;
-  sea_level: number;
-  grnd_level: number;
-  humidity: number;
-  temp_kf: number;
-}
-interface Weather {
-  id: number;
-  main: string;
-  description: string;
-  icon: string;
-}
-
-interface Clouds {
-  all: number;
-}
-interface Wind {
-  speed: number;
-  deg: number;
-  gust: number;
-}
-
-interface Sys {
-  pod: string;
-}
-interface ListItem {
+interface WeatherDetail {
   dt: number;
-  main: Main;
-  weather: Weather[];
-  clouds: Clouds;
-  wind: Wind;
+  main: {
+    temp: number;
+    feels_like: number;
+    temp_min: number;
+    temp_max: number;
+    pressure: number;
+    sea_level: number;
+    grnd_level: number;
+    humidity: number;
+    temp_kf: number;
+  };
+  weather: {
+    id: number;
+    main: string;
+    description: string;
+    icon: string;
+  }[];
+  clouds: {
+    all: number;
+  };
+  wind: {
+    speed: number;
+    deg: number;
+    gust: number;
+  };
   visibility: number;
   pop: number;
-  sys: Sys;
+  sys: {
+    pod: string;
+  };
   dt_txt: string;
-}
-interface Coord {
-  lat: number;
-  lon: number;
-}
-interface City {
-  id: number;
-  name: string;
-  coord: Coord;
-  country: string;
-  population: number;
-  timezone: number;
-  sunrise: number;
-  sunset: number;
 }
 interface WeatherData {
   cod: string;
   message: number;
   cnt: number;
-  list: ListItem[];
-  city: City;
+  list: WeatherDetail[];
+  city: {
+    id: number;
+    name: string;
+    coord: {
+      lat: number;
+      lon: number;
+    };
+    country: string;
+    population: number;
+    timezone: number;
+    sunrise: number;
+    sunset: number;
+  };
 }
 
 export default function Home() {
+  const { isPending, error, data } = useQuery({
+    queryKey: ["repoData"],
+    queryFn: async () => {
+      const { data } = await axios.get(
+        `https://api.openweathermap.org/data/2.5/forecast?q=keyport&appid=${process.env.NEXT_PUBLIC_API_KEY}`
+      );
+
+      return data;
+    },
+  });
+
+  console.log("data", data);
+
+  if (isPending) return "Loading...";
+
   return (
     <div className="flex flex-col gap-4 bg-gray-100 min-h-screen">
       <Navbar />
     </div>
   );
 }
-
