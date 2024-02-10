01309c8690d2ecb71253007351557fae50df70fa
Jared Cazel <jjcazel@gmail.com>
2024-02-09 19:50:05 -0500

    Create container and start displaying dynamic weather data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
 "use client";
 
+import Container from "@/components/Container";
 import Navbar from "@/components/Navbar";
+import { convertKelvinToFahrenheit } from "@/utils/convertKelvinToFarenheit";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
+import { format, fromUnixTime, parseISO } from "date-fns";
 
 // https://api.openweathermap.org/data/2.5/forecast?q=keyport&appid=0bf1bddb1098291128c414cdb6eec85c
 interface WeatherDetail {
@@ -71,13 +74,71 @@ export default function Home() {
     },
   });
 
-  console.log("data", data);
+  if (isPending)
+    return (
+      <div className="flex items-center min-h-screen justify-center">
+        <p className="animate-bounce">Loading...</p>
+      </div>
+    );
 
-  if (isPending) return "Loading...";
+  const mostCurrentData = data.list[0];
 
   return (
     <div className="flex flex-col gap-4 bg-gray-100 min-h-screen">
       <Navbar />
+      <main className="px-3 max-w-7xl mx-auto flex flex-col gap-9 w-full pb-10 pt-4">
+        {/* today's data */}
+        <section className="space-y-4">
+          <div className="space-y-2">
+            <h2 className="flex gap-1 text-2xl items-end">
+              <p>{format(parseISO(mostCurrentData.dt_txt ?? ""), "EEEE")}</p>
+              <p className="text-lg">
+                {format(parseISO(mostCurrentData.dt_txt ?? ""), "dd.mm.yyyy")}
+              </p>
+            </h2>
+            <Container className="gap-10 px-6 items-center">
+              <div className="flex flex-col px-4">
+                <span className="text-5xl">
+                  {convertKelvinToFahrenheit(
+                    mostCurrentData?.main.temp ?? 296.37
+                  )}
+                  °
+                </span>
+                <p className="text-xs space-x-1 whitespace-nowrap">
+                  <span>Feels like</span>
+                  <span>
+                    {convertKelvinToFahrenheit(
+                      mostCurrentData?.main.feels_like ?? 296.37
+                    )}
+                    °
+                  </span>
+                </p>
+                <p className="text-xs space-x-2">
+                  <span>
+                    {convertKelvinToFahrenheit(
+                      mostCurrentData?.main.temp_min ?? 0
+                    )}
+                    °↓
+                  </span>
+                  <span>
+                    {convertKelvinToFahrenheit(
+                      mostCurrentData?.main.temp_max ?? 0
+                    )}
+                    °↑
+                  </span>
+                </p>
+              </div>
+              <div className="flex gap-10 sm:gap-16 overflow-x-auto w-full justify-between pr-3">
+                {data.list.map((data: WeatherDetail, index: number) => (
+                  <div key={index} className="flex flex-col justify-between gap-2 items-center text-xs font-semibold"></div>
+                ))}
+              </div>
+            </Container>
+          </div>
+        </section>
+        {/* 7 day forecast */}
+        <section></section>
+      </main>
     </div>
   );
 }
---
+++ src/utils/convertKelvinToFarenheit.ts
@@ -0,0 +1,5 @@
+
+export function convertKelvinToFahrenheit(tempInKelvin: number): number {
+  const tempInFahrenheit = (tempInKelvin * 9) / 5 - 459.67;
+  return Math.floor(tempInFahrenheit); // Removes decimal part and keeps integer part
+}
